0c8d2f13a08fb5eff3831f63dacb18db61674283
007vedant <raghuvedant00@gmail.com>
2020-12-01 13:22:49 +0530

    added intro solutions

---
+++ Create Strings l.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7;
+const int N=2e5+5;
+
+
+int main() {
+	FASTIO;
+
+	vector<string> v;
+	string s; cin >> s;
+	sort(all(s));
+
+	do v.pb(s); while(next_permutation(all(s)));
+	cout << v.size() << endl;
+	for(string str: v) cout << str << endl;
+
+return 0;
+}
