07dbee88b3a6675dffc8c2e4b3a4c0e310d1a97a
007vedant <raghuvedant00@gmail.com>
2020-12-08 22:56:46 +0530

    added sols

---
+++ Sorting and Searching/Tasks_and_Deadlines.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repr(i, a, b) for(int i = a; i <= (b); ++i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7;
+const int N = 2e5+5;
+
+
+int main(){
+	FASTIO;
+
+	int n, a, b;
+	cin >> n;
+	vpi arr(n);
+
+	rep(i, 0, n) {
+		cin >> a >> b;
+		arr[i] = mp(a, b);
+	}
+
+	sort(all(arr));
+
+	ll total = 0, curr = 0;
+
+	for (auto ele : arr){
+		curr += ele.f;
+		total += ele.s - curr;
+
+	}
+	cout << total;
+	return 0;
+}
