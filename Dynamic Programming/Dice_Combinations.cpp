e4f65c435d5ac5485596bd594e5ad52c49edd046
007vedant <raghuvedant00@gmail.com>
2020-12-09 19:37:25 +0530

    added dp sols

---
+++ Dynamic Programming/Dice_Combinations.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repr(i, a, b) for(int i = a; i <= (b); ++i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7;
+const int N = 1e6+6;
+
+int dp[N];
+
+int main() {
+	FASTIO;
+	dp[0] = 1;
+
+	int n; cin >> n;
+	repr(i, 1, n)
+		rep(j, max(0, i-6), i)
+			dp[i] = (dp[i] + dp[j]) % MOD;
+
+
+	cout << dp[n];
+	
+
+return 0;
+}
