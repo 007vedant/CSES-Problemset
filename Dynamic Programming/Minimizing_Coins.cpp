3e2e541a230fc9732177c2de909bbd5b586fbd25
007vedant <raghuvedant00@gmail.com>
2020-12-09 19:36:12 +0530

    added dp sols

---
+++ Dynamic Programming/Minimizing_Coins.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repr(i, a, b) for(int i = a; i <= (b); ++i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7;
+const int N = 2e5+5;
+
+
+int main() {
+	FASTIO;
+
+	int n, x; cin >> n >> x;
+	vi coins(n);
+
+	for(auto &c: coins) cin >> c;
+
+	vi dp(x+1, MOD);
+
+	dp[0] = 0;
+
+	repr(i, 1, x)
+		rep(j, 0, n)
+			if(i - coins[j] >= 0)
+				dp[i] = min(dp[i], dp[i - coins[j]]+1);
+
+
+	cout << (dp[x] == MOD ? -1: dp[x]);
+
+	
+
+return 0;
+}
