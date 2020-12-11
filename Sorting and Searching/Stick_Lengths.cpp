3b91746a01e21c6c10c2c407d8c344eb70cf6e42
007vedant <raghuvedant00@gmail.com>
2020-12-12 02:04:54 +0530

    added sols

---
+++ Sorting and Searching/Stick_Lengths.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repr(i, a, b) for(int i = a; i <= (b); ++i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7;
+const int N = 2e5+5;
+
+
+int main() {
+	FASTIO;
+
+	int n; cin >> n;
+	vl sticks(n);
+
+	for(auto &x: sticks) cin >> x;
+
+	sort(all(sticks));
+	
+	ll mid = sticks[n/2];
+
+	ll ans = 0;
+
+	for(int x: sticks)
+		ans += abs(x - mid);
+
+	cout << ans;
+	
+
+return 0;
+}
