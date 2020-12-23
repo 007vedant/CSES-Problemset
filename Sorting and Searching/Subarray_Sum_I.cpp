c380fd7be6a7ce4e6de1e0183ed89d6b951be12e
007vedant <raghuvedant00@gmail.com>
2020-12-23 21:32:23 +0530

    added s&s sols

---
+++ Sorting and Searching/Subarray_Sum_I.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repr(i, a, b) for(int i = a; i <= (b); ++i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7;
+const int N = 2e5+5;
+
+
+int main() {
+	FASTIO;
+
+	int n, x; cin >> n >> x;
+	vi arr(n);
+	for(auto &x: arr) cin >> x;
+
+	unordered_map<ll, int> mp;
+	int ans = 0; 
+	ll curr = 0;
+
+	rep(i, 0, n){
+		curr += arr[i];
+		if(curr == x) ans++;
+
+		if(mp.find(curr-x) != mp.end())
+			ans += mp[curr-x];
+
+		mp[curr]++;
+	}
+
+	cout << ans;
+	
+
+return 0;
+}
