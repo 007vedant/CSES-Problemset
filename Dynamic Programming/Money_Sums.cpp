d62d13862de76587aeb2ec5caf8077791ec12ce3
007vedant <raghuvedant00@gmail.com>
2021-01-04 19:27:03 +0530

    added dp sols

---
+++ Dynamic Programming/Money_Sums.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std; 
+using namespace __gnu_pbds; 
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> Tree;
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repr(i, a, b) for(int i = a; i <= (b); ++i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7;
+const int N = 2e5+5;
+
+
+int main() {
+	FASTIO;
+
+	int n; cin >> n;
+
+	set<int> ans;
+
+	while(n--){
+		int x; cin >> x;
+		if(ans.empty()) { 
+			ans.ins(x);
+			continue;
+		}
+
+		vi curr;
+		for(auto t: ans)
+			curr.pb(t+x);
+
+		ans.ins(x);
+		for(auto t: curr)
+			ans.ins(t);
+
+	}
+
+	cout << sz(ans) << endl;
+	for(auto t: ans) cout << t << " ";
+
+
+
+return 0;
+}
