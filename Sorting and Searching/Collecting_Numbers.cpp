ca037520f44d1c913dedf9690350d4551cad98c7
007vedant <raghuvedant00@gmail.com>
2021-01-16 23:15:14 +0530

    added sol for SoS problems

---
+++ Sorting and Searching/Collecting_Numbers.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std; 
+using namespace __gnu_pbds; 
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> Tree;
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repr(i, a, b) for(int i = a; i >= (b); --i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+const int MOD = 1e9+7;
+const int N = 2e5+5;
+
+int vis[N+1];
+
+
+int main() {
+	FASTIO;
+	int n, x; cin >> n;
+	int ans = 0;
+	while(n--) {
+		cin >> x;
+		if(vis[x-1] == 0) ans += 1;
+		vis[x]++;
+	}
+
+	cout << ans;
+
+
+
+
+return 0;
+}
