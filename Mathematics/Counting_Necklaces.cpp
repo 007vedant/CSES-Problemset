9e7b70c2fac6232e2d31e5829cd6a023b6067aea
007vedant <raghuvedant00@gmail.com>
2021-04-09 20:07:37 +0530

    added sols for mathematics section

---
+++ Mathematics/Counting_Necklaces.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std; 
+using namespace __gnu_pbds; 
+
+typedef long long ll;
+typedef long double ld;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> Tree;
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repr(i, a, b) for(int i = a; i >= (b); --i)
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+#define INF    0x3f3f3f3f3f3f3f3fLL
+
+
+const int MOD = 1e9+7;
+const int N = 2e5+5;
+
+// BURNSIDE'S LEMMA
+
+
+
+ll binexp(ll a, ll n){
+    ll res = 1;
+    while(n > 0){
+        if(n & 1)
+        	res = (res * a);
+        a = (a * a);
+        n >>= 1;
+    }
+    return res;
+}
+
+
+
+int main() {
+	FASTIO;
+	int n, m; cin >> n >> m;
+	ll ans = 0;
+	rep(i, 0, n){
+		ans = (ans + binexp(m, __gcd(i, n)));
+	}
+
+	ans /= n;
+
+	cout << ans%MOD;
+	
+
+return 0;
+}
